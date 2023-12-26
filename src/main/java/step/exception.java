caa6308649c534714f4f9501426fd05cb44512fa
김종만 <v666v@naver.com>
2023-12-27 00:46:12 +0900

    first commit

--- src/main/java/step/exception.java
+++ src/main/java/step/exception.java
@@ -2,7 +2,7 @@ package step;
 
 public class exception {
     public static void main(String [] args){
-        System.out.println("abc!!!!!!!!!!!!!");
+        System.out.println("abc!!!!!!!!!!!!!!!!!");
     }
 
 }
